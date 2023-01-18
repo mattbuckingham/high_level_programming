4bd681d13327b365632536ef45390d25b2785ced
bigBadMatt <5707@holbertonstudents.com>
2023-01-18 14:05:39 -0800

    removed commented-out code block

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,16 +3,3 @@ for i in range(1, 99):
     if (i % 10) >= int(i / 10):
         print("{:02}, ".format(i), end="")
 print("99")
-
-
-
-""""
-i = 1
-while i <= 99:
-    print("{:02}, ".format(i), end="")
-    if (i % 10) % 9 == 0:
-        i = i + int(i / 10)
-    else:
-        i = i + 1
-print("99")
-"""
