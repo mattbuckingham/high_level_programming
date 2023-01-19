5cde49de4f0922b645fcbce7cd4e2a552e63f8e4
bigBadMatt <5707@holbertonstudents.com>
2023-01-19 14:57:10 -0800

    program that counts the number of arguments passed to it

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    args = len(sys.argv) - 1
+
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args))
+    for i in range(args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
