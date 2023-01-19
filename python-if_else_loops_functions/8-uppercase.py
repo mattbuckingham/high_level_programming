429c2fd533b64cf1b4b83cc8c7ae5cee8a168858
bigBadMatt <5707@holbertonstudents.com>
2023-01-18 16:33:00 -0800

    function to print a sting in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            upper = chr(ord(i) - 32)
+        else:
+            upper = i;
+        print(upper, end = "")
+    print()
