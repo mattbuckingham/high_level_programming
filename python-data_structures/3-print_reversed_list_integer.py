22446d4e3bdb5a26854a5a33f455cbb2ff763502
bigBadMatt <5707@holbertonstudents.com>
2023-01-24 15:59:09 -0800

    added check for case that list == None

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def print_reversed_list_integer_recursion(idx, my_list=[]):
     if idx == 0:
         return
@@ -8,4 +7,6 @@ def print_reversed_list_integer_recursion(idx, my_list=[]):
     print_reversed_list_integer_recursion(idx - 1, my_list)
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return
     print_reversed_list_integer_recursion(len(my_list), my_list)
