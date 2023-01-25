3462e7fbff8763ed3b19ecdd6ea49dca8373aa11
bigBadMatt <5707@holbertonstudents.com>
2023-01-24 16:00:51 -0800

    fixed formatting to satisfy linter

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+
 def print_reversed_list_integer_recursion(idx, my_list=[]):
     if idx == 0:
         return
     print("{:d}".format(my_list[(idx-1)]))
     print_reversed_list_integer_recursion(idx - 1, my_list)
 
+
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None:
         return
     print_reversed_list_integer_recursion(len(my_list), my_list)
