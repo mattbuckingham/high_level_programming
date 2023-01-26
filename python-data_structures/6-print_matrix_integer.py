ccbccbdfe83cedb861f7d1c8127b146225d521d8
bigBadMatt <5707@holbertonstudents.com>
2023-01-25 22:00:27 -0800

    fixed print statement to terminate without space char

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end="")
+            if element is not row[len(row) - 1]:
+                print("{:d}".format(element), end=" ")
+            else:
+                print("{:d}".format(element), end="")
         print()
