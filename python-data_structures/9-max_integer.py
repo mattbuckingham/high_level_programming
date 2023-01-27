1b984936ce23fe5f43f651e92ea2fb41d3751202
bigBadMatt <5707@holbertonstudents.com>
2023-01-27 02:03:49 -0800

    added edgecase for passing a empty list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     max_val = float("-inf")
     for i, value in enumerate(my_list):
         if max_val < value:
