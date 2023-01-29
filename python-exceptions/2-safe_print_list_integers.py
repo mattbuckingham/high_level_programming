4f41951cb4b3d44458b938eb06e6f8cee69539fe
bigBadMatt <5707@holbertonstudents.com>
2023-01-29 02:20:57 -0800

    reworked function so that sn exception was being thrown when passing an x bigger than the size to be printed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             if type(my_list[i]) == int:
                 print("{:d}".format(my_list[i]), end="")
                 count = count + 1
-        print()
-    except:
-        return count
+        except:
+            return
+    print()
     return count
