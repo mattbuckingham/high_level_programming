8401a198db1ae1878ddd5a054c9101331550a130
bigBadMatt <5707@holbertonstudents.com>
2023-01-29 04:46:11 -0800

    fixed formatting to satisfy linter

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,8 @@ def roman_to_int(roman_string):
                       'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for i in range(len(roman_string)):
-        if i > 0 and \
-        roman_numerals[roman_string[i]] > roman_numerals[roman_string[i - 1]]:
+        if i > 0 and roman_numerals[roman_string[i]] >\
+           roman_numerals[roman_string[i - 1]]:
             result = result + (roman_numerals[roman_string[i]] - 2 *
                                roman_numerals[roman_string[i - 1]])
         else:
