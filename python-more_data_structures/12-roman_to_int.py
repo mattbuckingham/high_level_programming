f86cd5fed6d0b3b9b0a8c6eef15f6516257ce8bf
bigBadMatt <5707@holbertonstudents.com>
2023-01-29 03:36:29 -0800

    moved to correct dir

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) != str or roman_string == None:
+        return 0
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_numerals[roman_string[i]] > roman_numerals[roman_string[i -1]]:
+            result = result + (roman_numerals[roman_string[i]] -2 * roman_numerals[roman_string[i -1]])
+        else:
+            result = result + roman_numerals[roman_string[i]]
+    return result
