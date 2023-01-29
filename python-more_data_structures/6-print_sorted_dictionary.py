4881e81cea4b8b1bd3b7664447c873046d4f0da2
bigBadMatt <5707@holbertonstudents.com>
2023-01-28 21:23:00 -0800

    fixed formatting to satisfy linter

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        a_dictionary = {}
     for key in sorted(a_dictionary.keys()):
         print(key, end="")
         print(":", a_dictionary[key])
