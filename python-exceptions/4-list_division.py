64fe04fc49a1683074851ea4fc87889203b75e29
bigBadMatt <5707@holbertonstudents.com>
2023-01-29 04:28:16 -0800

    fixed formatting to satisfy linter

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
+    new_list = []
     for i in range(list_length):
         try:
-            if i >= len(my_list_1) or i >= len (my_list_2):
-                raise range
-            elif type(my_list_1[i]) is not int or type(my_list_2[i]) is not int:
-                raise type
-            elif my_list_2[i] == 0:
-                raise zero_div
-            else:
-                result.append(my_list_1[i]/my_list_2[i])
-        except range:
+            result = my_list_1[i]/my_list_2[i]
+        except IndexError:
             print("out of range")
-            result.append(0)
-        except type:
+            result = 0
+        except TypeError:
             print("wrong type")
-            result.append(0)
-        except zero_div:
+            result = 0
+        except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
+            result =0
         finally:
-
+            result = result
     return result
