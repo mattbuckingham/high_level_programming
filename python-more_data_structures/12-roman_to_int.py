4dabe674635241f99b30e925150d708db0d56105
bigBadMatt <5707@holbertonstudents.com>
2023-01-29 04:37:28 -0800

    fixed formatting to satisfy linter

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or roman_string is None:
         return 0
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_numerals[roman_string[i]] > roman_numerals[roman_string[i -1]]:
-            result = result + (roman_numerals[roman_string[i]] -2 * roman_numerals[roman_string[i -1]])
+        if i > 0 and roman_numerals[roman_string[i]] > roman_numerals[roman_string[i - 1]]:
+            result = result + (roman_numerals[roman_string[i]] -2 * roman_numerals[roman_string[i - 1]])
         else:
             result = result + roman_numerals[roman_string[i]]
     return result
