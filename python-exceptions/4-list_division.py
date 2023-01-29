da65d32abffb83db65b2d6d42c81012b254dfef5
bigBadMatt <5707@holbertonstudents.com>
2023-01-29 04:33:08 -0800

    fixed formatting to satisfy linter

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except ZeroDivisionError:
             print("division by 0")
-            result =0
+            result = 0
         finally:
             new_list.append(result)
     return new_list
