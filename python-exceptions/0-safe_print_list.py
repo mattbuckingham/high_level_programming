c351c73b927ace8da3fb1de26c41b72ea9ac8be3
bigBadMatt <5707@holbertonstudents.com>
2023-01-28 23:47:43 -0800

    fixed formatting to satisfy linter

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             count = count + 1
         print()
     except IndexError:
