1400d00d807bd740646e32cf7f192784519acaeb
bigBadMatt <5707@holbertonstudents.com>
2023-01-31 14:34:35 -0800

    a square class with the size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class that defines a square"""
+
+
+class Square:
+    """a square"""
+    def __init__(self, size):
+        """ Initialise a new square"""
+        self.size = size
