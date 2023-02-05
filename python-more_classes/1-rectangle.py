8d98a25f1dcc2c15958c37c64170df3492263cbb
bigBadMatt <5707@holbertonstudents.com>
2023-02-05 13:40:15 -0800

    fixed formatting to satisfy linter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        self.__witdth = width
+        self.__width = width
 
     @property
     def height(self):
