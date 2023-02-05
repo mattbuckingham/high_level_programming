37a3a2d471483669479589fcca5742968ab24457
bigBadMatt <5707@holbertonstudents.com>
2023-02-05 04:37:27 -0800

    more fixes to error checking

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     """
     adds two integers, if either are floats they are converted
     """
-    if type(a) is not float or not int:
+    if type(a) is not float or type(a) is not int:
         raise TypeError("a must be an integer")
-    if type(b) is not float or not int:
+    if type(b) is not float or type(a) is not int:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
