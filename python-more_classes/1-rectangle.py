28c25ef721c1365c4b7f09422e767d450a2cf7b3
bigBadMatt <5707@holbertonstudents.com>
2023-02-05 13:42:03 -0800

    removed return statement from setter method, no idea why i put it there

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -57,4 +57,4 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        return self.__width
+        self.__width = value
