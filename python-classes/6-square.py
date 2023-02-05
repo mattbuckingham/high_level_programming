66e488d366204d992ab1bff098926fc550e37b35
bigBadMatt <5707@holbertonstudents.com>
2023-02-05 03:58:16 -0800

    fixed formatting to satisfy linter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -68,8 +68,9 @@ tuple of 2 positive integers")
             type(value[0]) != int or
             type(value[1]) != int or
             value[0] < 0 or
-            value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
+                value[1] < 0):
+                raise TypeError("position must be a \
+tuple of 2 positive integers")
         else:
             self.__position = value
 
