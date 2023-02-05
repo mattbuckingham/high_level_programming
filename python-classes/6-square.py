6c2985534c029743ab790fa05216c1d359c33158
bigBadMatt <5707@holbertonstudents.com>
2023-02-05 03:48:30 -0800

    fixed formatting to satisfy linter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -26,7 +26,7 @@ class Square:
             type(position[1]) != int or
             position[0] < 0 or
             position[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
+                raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
 
