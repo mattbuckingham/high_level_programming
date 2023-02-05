5add78e58ce7c0e44be7551eeecb34c3f0526221
bigBadMatt <5707@holbertonstudents.com>
2023-02-05 03:49:47 -0800

    fixed formatting to satisfy linter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -21,12 +21,12 @@ class Square:
             self.__size = size
 
         if (type(position) != tuple or
-            len(position) != 2 or
-            type(position[0]) != int or
-            type(position[1]) != int or
-            position[0] < 0 or
-            position[1] < 0):
-                raise TypeError("position must be a tuple of 2 positive integers")
+        len(position) != 2 or
+        type(position[0]) != int or
+        type(position[1]) != int or
+        position[0] < 0 or
+        position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
 
