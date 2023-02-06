a4d8b7e8e222455eeeb2d22960373c1666f41dd9
bigBadMatt <5707@holbertonstudents.com>
2023-02-06 14:30:55 -0800

    fixed formatting to satisfy linter

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def lookup(obj):
-"""
-Method that returns a list of all attributes and methods available to obj
-"""
+    """
+    Module that returns a list of all attributes and methods available to obj
+    """
+
     return [dir(obj)]
