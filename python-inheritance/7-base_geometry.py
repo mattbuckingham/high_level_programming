c6e65596778c59f3c9be91af835d2bc6a6c28b2f
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 02:53:59 -0800

    fixed error to trigger when int is 0

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,5 +14,5 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
