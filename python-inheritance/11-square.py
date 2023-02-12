869ed50f0a5af8c24c6faa36b846076cc13c7ba1
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 04:01:50 -0800

    fixed formatting to satisfy linter

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,6 +15,5 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
