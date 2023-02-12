b14c9591f09d53f3ab8fd5cdebffc511eb8440dd
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 03:40:50 -0800

    added call to parent class __init__

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -30,6 +30,7 @@ class Square(Rectangle):
 
         self.integer_validator("size", size)
         self.__size = size
+        Rectangle.__init__(self, size, size)
 
     def area(self):
         return self._size ** 2
