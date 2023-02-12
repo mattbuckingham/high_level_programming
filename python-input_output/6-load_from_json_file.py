472de64122caf8f942b28f0ab66046c641d0eee5
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 04:33:21 -0800

    fixed typo in with arg

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
     obj from json
     """
-    with open(filename, "r", ebncoding="utf-8") as myfile:
+    with open(filename, "r", encoding="utf-8") as myfile:
         return json.load(myfile)
