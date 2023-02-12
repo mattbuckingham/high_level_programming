8215cca679e5ba0f26c4014bd21d76e1ef90ae31
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 03:24:22 -0800

    changed class to import from local file rather than defining both classes in a single file

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
 """
-Class for a geometric object
+Class for a rectangle object
 """
 
 
-class BaseGeometry:
-    """
-    Geometeric object
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry)
 
 class Rectangle(BaseGeomerty):
     """
