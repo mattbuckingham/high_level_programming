0df3067c885e8858ea92b20ecb3cf049eca9122f
bigBadMatt <5707@holbertonstudents.com>
2023-02-11 22:58:32 -0800

    added mising interpreter prompt to testfile

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,7 +12,7 @@ def matrix_divided(matrix, div):
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisonError("division by zero")
+        raise ZeroDivisionError("division by zero")
     for row in matrix:
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
