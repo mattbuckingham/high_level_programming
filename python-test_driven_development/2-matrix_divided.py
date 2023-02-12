d301e684bc072765040eeaaa8051a01369330f08
bigBadMatt <5707@holbertonstudents.com>
2023-02-11 22:43:57 -0800

    a function that divides all elements of a matrix and testfile

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module divides all elements of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    function that divides all elements of a matrix
+    """
+
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisonError("division by zero")
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for elem in row:
+            if type not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+
+    return [[round(elem/div, 2) for elem in row] for row in matrix]
