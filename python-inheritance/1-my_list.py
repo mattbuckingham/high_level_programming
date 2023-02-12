8bdf7e68cd0f1dd34b76087d6d4cc4c3c4cac573
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 01:30:34 -0800

    a class that inherits from list and has a method to sort a list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Sorts a list
+"""
+
+class MyList(list):
+    """
+    Sorts a list
+    """
+
+    def print_sorted(self):
+        """
+        method to sort a list
+        """
+        temp_list = sorted(self)
+        print(temp_list)
