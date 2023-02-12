733ac89d6f950416b712ae90575d446d7bc79b8d
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 03:56:43 -0800

    added area and str methods

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Class for a rectangle object
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square Object
+    """
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
