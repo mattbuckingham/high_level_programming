9f5ab9e216d5fad76856dfc5c461cf863faf6e9f
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 02:02:10 -0800

    added descriptions

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+checks if an object is a specific class
+"""
+
+
 def is_same_class(obj, a_class):
+    """
+    Returns true if an object is a class
+    """
     return (type(obj) is a_class)
