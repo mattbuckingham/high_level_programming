85bcacbdc7ca827105fef0f4cf81561e70026af2
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 02:21:59 -0800

    BaseGeometry class with area method that raises an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class for a geometric object
+"""
+
+
+class BaseGeometry:
+    """
+    Geometeric object
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
