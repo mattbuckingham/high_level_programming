729353adc13f946c062bbebfff493de19269ec0d
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 21:37:02 -0800

    unction that prints a square with the character #

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Prints a square using #
+"""
+
+
+def print_square(size):
+    """
+    Print a square
+    """
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is no int:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        print("#" * size + "\n")
