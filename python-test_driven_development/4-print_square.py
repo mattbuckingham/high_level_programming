9640779a66ebb0ae251043e98b11bb8af8154b4c
bigBadMatt <5707@holbertonstudents.com>
2023-02-12 21:42:00 -0800

    rearranged error checks

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,10 +9,10 @@ def print_square(size):
     Print a square
     """
 
-    if size < 0:
-        raise ValueError("size must be >= 0")
     if type(size) is not int:
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
-        print("#" * size + "\n")
+        print("#" * size)
