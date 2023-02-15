b6d19c94b6019e25ea7298a7b1fb4cf5097604c8
bigBadMatt <5707@holbertonstudents.com>
2023-02-15 15:59:39 -0800

    function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+prints text and inserts newlines
+"""
+
+
+def text_indentation(text):
+    """
+    function that prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+
+    previous_char = None
+    for char in text:
+        if char in ["?", ":", "."]:
+            print(f"{char} \n")
+            previous_char = "\n"
+        elif char == " " and previous_char == "\n":
+            pass
+        else:
+            print(char, end="")
+            previous_char = char
