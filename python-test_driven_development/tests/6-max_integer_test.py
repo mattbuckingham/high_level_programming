40a96e5087055270a461f3aefe5fd71d79749ed2
bigBadMatt <5707@holbertonstudents.com>
2023-02-17 03:03:05 -0800

    changed type of unit test parameters

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,25 +10,25 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_MaxEnd(self):
-        self.assertEqual(max_integer([1, 1, 1, 2]), '2')
+        self.assertEqual(max_integer([1, 1, 1, 2]), 2)
 
     def test_MaxStart(self):
-        self.assertEqual(max_integer([2, 1, 1, 1]), '2')
+        self.assertEqual(max_integer([2, 1, 1, 1]), 2)
 
     def test_MaxMid(self):
-        self.assertEqual(max_integer([1, 2, 1]), '2')
+        self.assertEqual(max_integer([1, 2, 1]), 2)
 
     def test_OneNegative(self):
-        self.assertEqual(max_integer([1, 1, -1, 2]), '2')
+        self.assertEqual(max_integer([1, 1, -1, 2]), 2)
 
     def test_OneNegative(self):
-        self.assertEqual(max_integer([-1, -1, -1, -2]), '1')
+        self.assertEqual(max_integer([-1, -1, -1, -2]), 1)
 
     def test_OneArg(self):
-        self.assertEqual(max_integer([1]), '1')
+        self.assertEqual(max_integer([1]), 1)
 
     def test_NoArg(self):
-        self.assertEqual(max_integer(), 'None')
+        self.assertEqual(max_integer(), None)
 
 if __name__=='__main__':
     unittest.main()
