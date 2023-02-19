d60f5518b0be3179b4df838f28973f66233a7d40
bigBadMatt <5707@holbertonstudents.com>
2023-02-19 01:35:02 -0800

    fixed raising errors of the wrong type

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise TypeError("width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -50,7 +50,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise TypeError("height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -68,7 +68,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise TypeError("x must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -86,7 +86,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise TypeError("y must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
