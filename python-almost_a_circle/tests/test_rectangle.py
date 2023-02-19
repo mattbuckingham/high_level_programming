79cafbb62c2da9aae19777ec6aeefd7c19ab1659
bigBadMatt <5707@holbertonstudents.com>
2023-02-19 03:36:48 -0800

    added triangle tests

---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+import unittest
+from models import rectangle
+from models.base import Base
+Rectangle = rectangle.Rectangle
+
+class TestRectangle(unittest.TestCase):
+    """
+
+    """
+
+    def test_2_arg(self):
+        """
+        make a simple rec, test width
+        """
+        r = Rectangle(1, 2)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+
+    def test_3_arg(self):
+        """
+        rec with an x value
+        """
+        r1 = Rectangle(1, 2, 3)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+
+    def test_4_arg(self):
+        """
+        rec with a y value
+        """
+        r2 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r2.y, 4)
