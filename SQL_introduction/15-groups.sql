98d2d9ac02ae65968260da8318ded0d627909f2d
bigBadMatt <5707@holbertonstudents.com>
2023-03-18 17:14:52 -0700

    added statement to order the result

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table
-SELECT score, count(*) AS 'number' FROM second_table GROUP BY score
+SELECT score, count(*) AS 'number' FROM second_table GROUP BY score ORDER BY score DESC
