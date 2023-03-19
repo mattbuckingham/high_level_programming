4d73ba630ec114a7e86da2ed6468e7f376fbdfd0
bigBadMatt <5707@holbertonstudents.com>
2023-03-19 05:46:34 -0700

    fixed formatting to satisfy linter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     )
 
     cursor = dbase.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     results = cursor.fetchall()
 
     for row in results:
