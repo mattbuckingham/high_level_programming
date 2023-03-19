64cccec673fc4d60a773c2ecd2e144f656b17d09
bigBadMatt <5707@holbertonstudents.com>
2023-03-19 05:59:13 -0700

    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe fr

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    dbase = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    state = sys.argv[4]
+    cursor = dbase.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name =%s", (state, ))
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
