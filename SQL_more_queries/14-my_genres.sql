2f4e4e7f9fc660b1b8f9e09ea43a7c551915866b
bigBadMatt <5707@holbertonstudents.com>
2023-03-19 04:18:21 -0700

    fixed typo in column name

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT tv_genres.name
-FROM tv_shows_genres
+FROM tv_show_genres
 LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
