504f94645b7e2eda87fca7019a04b941b6872636
bigBadMatt <5707@holbertonstudents.com>
2023-03-19 05:30:22 -0700

    added interface with mysql and logic to print output

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,3 +2,23 @@
 """
 
 """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    dbase = MySQL.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    result = cursor.fetchall()
+
+    for row in results:
+        print(row)
