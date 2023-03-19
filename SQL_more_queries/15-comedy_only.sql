cad051e64ccba88e8f8895f6ba770ea45c150a60
bigBadMatt <5707@holbertonstudents.com>
2023-03-19 04:30:13 -0700

    lists all Comedy shows in the database hbtn_0d_tvshows

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -6,4 +6,4 @@ ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy"
-ORDER BY tv_show.title ASC;
+ORDER BY tv_shows.title ASC;
