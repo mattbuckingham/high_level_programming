fc0e634761192cbf134dc7c6f02b90d8ada3adfc
bigBadMatt <5707@holbertonstudents.com>
2023-03-29 17:33:07 -0700

    takes in the name of a state as an argument and lists all cities of that state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+takes in the name of a state as an argument and lists all cities of that state
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    takes in the name of a state as an argument and lists
+    all cities of that state
+    """
+
+    dbase = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    state = sys.argv[4]
+
+    cursor = dbase.cursor()
+    SQL = "SELECT cities.name FROM cities INNER JOIN states ON cities.state_id =
+states.id WHERE states.name = '{}'".format(state)
+    cursor.execute(SQL)
+    rows=cursor.fetchall()
+    results=[]
+    for row in rows:
+        for col in row:
+            results.append(col)
+    print(','.join(results))
