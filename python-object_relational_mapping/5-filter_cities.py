841ee7812e9a42ba05256fbf57b5a76316ad04ba
bigBadMatt <5707@holbertonstudents.com>
2023-03-29 18:24:52 -0700

    fixed formatting to satisfy linter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,9 +7,6 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """
-    takes in the name of a state as an argument and lists all cities
-    """
 
     dbase = MySQLdb.connect(
         host="localhost",
@@ -19,19 +16,13 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-    state_str = sys.argv[4]
-    state_list = state_str.split(';')
-    state_list = state_list[0].replace("\'","")
+    state_list = sys.argv[4]
 
     cursor = dbase.cursor()
     SQL = "SELECT cities.name FROM cities"
     JOIN = "INNER JOIN states ON cities.state_id = states.id"
     WHERE = "WHERE states.name = '{}'".format(state_list)
-    SQL_query = "{} {} {}".format(SQL, JOIN, WHERE)
+    SQL_query = "{} {} {};".format(SQL, JOIN, WHERE)
     cursor.execute(SQL_query, (state_list,))
-    rows=cursor.fetchall()
-    results=[]
-    for row in rows:
-        for col in row:
-            results.append(col)
-    print(','.join(results))
+    results=cursor.fetchall()
+    print(','.join([state[1] for state in results]))
