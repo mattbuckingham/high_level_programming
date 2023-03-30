75fd717dda56446194e43c278a79d0fc852d6577
bigBadMatt <5707@holbertonstudents.com>
2023-03-29 19:44:31 -0700

    fixed formatting to satisfy linter

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,6 +13,6 @@ class State(Base):
     """
     State class
     """
-    __tablename__='states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
