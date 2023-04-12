283e09f09628d9ac4cccde487bbfccd77a970788
bigBadMatt <5707@holbertonstudents.com>
2023-04-12 15:11:13 -0700

    prints a message depending of the number of arguments passed

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length === 2){
+    console.log('No arguments');
+}
+if (process.argv.length === 3){
+    console.log('Argument found');
+}
+if (process.argv.length === 4){
+    console.log('Arguments found');
+}
