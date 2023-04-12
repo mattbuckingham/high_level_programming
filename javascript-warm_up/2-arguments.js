331302f8e14750aa92cf8ff58430225c30968780
bigBadMatt <5707@holbertonstudents.com>
2023-04-12 15:18:00 -0700

    fixed formatting to satisfy linter

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-if (process.argv.length === 2){
-    console.log('No argument');
+if (process.argv.length === 2)
+{
+  console.log('No argument');
 }
-if (process.argv.length === 3){
-    console.log('Argument found');
+if (process.argv.length === 3)
+{
+  console.log('Argument found');
 }
-if (process.argv.length >= 4){
-    console.log('Arguments found');
+if (process.argv.length >= 4)
+{
+  console.log('Arguments found');
 }
