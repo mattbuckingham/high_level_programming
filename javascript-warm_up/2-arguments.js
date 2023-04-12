f742a47f22e4473aa1b19af5326ddfa21819e822
bigBadMatt <5707@holbertonstudents.com>
2023-04-12 15:16:05 -0700

    fixed syntax error

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,6 +5,6 @@ if (process.argv.length === 2){
 if (process.argv.length === 3){
     console.log('Argument found');
 }
-if (process.argv.length ==< 4){
+if (process.argv.length >= 4){
     console.log('Arguments found');
 }
