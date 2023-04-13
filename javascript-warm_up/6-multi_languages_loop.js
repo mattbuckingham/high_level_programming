e7fc8ca041ce72a2e73c6577beacfbb91c26e29c
bigBadMatt <5707@holbertonstudents.com>
2023-04-12 17:53:12 -0700

    fixed formatting to satisfy linter

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,8 @@
 
 const prints = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (i=0; i < prints.length; i++) {
-    console.log(prints[i]);
+let i;
+
+for (i = 0; i < prints.length; i++) {
+  console.log(prints[i]);
 }
