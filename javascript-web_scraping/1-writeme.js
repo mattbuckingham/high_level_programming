27d8b1f0ebf04c8388c27f502682c44d8f61a273
bigBadMatt <5707@holbertonstudents.com>
2023-04-18 21:58:31 -0700

    fixed broken syntax

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function(err, data){
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
     if (err){
 	console.log(err);
     }
-    console.log(data);
 });
