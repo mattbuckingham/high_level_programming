2ebd60382ea19dd9bec6609221b1b5845e2fd3e6
bigBadMatt <5707@holbertonstudents.com>
2023-04-20 19:51:15 -0700

    fixed formatting to satisfy linter

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(err, data){
-    if (err){
-	console.log(err);
-    }
-    console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
 });
