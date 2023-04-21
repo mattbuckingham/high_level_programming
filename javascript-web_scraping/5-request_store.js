217a39f8505e00d41fca23d242115e2938fc51fb
bigBadMatt <5707@holbertonstudents.com>
2023-04-20 20:54:27 -0700

    fixed formatting to satisfy linter

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,13 +5,13 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
-	console.log(error);
-    } else {
-	fs.writeFile(process.argv[3], body, 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', function (error, data) {
       if (error) {
         console.log(error);
       }
     });
-    }
+  }
 });
