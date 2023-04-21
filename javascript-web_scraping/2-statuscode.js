4d0c88fa19ad00096302b76188c37287cc03b5bb
bigBadMatt <5707@holbertonstudents.com>
2023-04-20 20:00:17 -0700

    fixed formatting to satisfy linter

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-
 const process = require('process');
 const request = require('request');
 const url = process.argv[2];
-
 
 request.get(url).on('response', (response) => {
   console.log(`code: ${response.statusCode}`);
